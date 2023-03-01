f764063faedd097661b6bb3e9cf4e47e9a0896bd
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-01 07:22:07 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App/>
+    <Helmet>
+      <title>Abyansyah Ahmad</title>
+      <meta name="description" content="Abyansyah Ahmad" />
+    </Helmet>
+    <App />
   </BrowserRouter>
 );
